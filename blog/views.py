ff5f1ce45ac7042ab9b4b590a05001bde97ac1d9
ozlemezgi <ozlemezgisari@icloud.com>
2021-10-22 17:34:53 +0300

    migrations and user registration page

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-
-posts=[
-    {
-        'author': 'ozlem ezgi sari',
-        'title': 'First Blog Post',
-        'content':'trial post',
-        'date_posted':'October 19 , 2021 ',
-    },
-    {
-        'author':'volkan aktaş',
-        'title':'Hello Friends,Welcome to My Channel',
-        'content':'How to get rich from Bitcoin',
-        'date_posted':'October 20 , 2021 ',
-    }
-   
-]
-
+from .models import Post
 
 
 def home(request):
     context={
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html',context)
 
--- django_project/settings.py
+++ django_project/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
     'blog',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,5 +124,7 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
