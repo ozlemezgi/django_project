efbab151e23bf8dbee1c0667d6611c127ea82c38
ozlemezgi <ozlemezgisari@icloud.com>
2022-03-21 16:38:01 +0200

    pagination and profile post page

--- blog/urls.py
+++ blog/urls.py
@@ -2,13 +2,16 @@ from django.urls import path
 from .views import (
                     PostListView,
                     PostDetailView,             
-                    PostDeleteView
+                    PostDeleteView,
+                    UserPostListView
                     )
 from . import views
 
 app_name = "blog"
 urlpatterns = [
     path('post/update/<int:pk>/',views.postUpdate,name='post-update'),
+    path('user/<str:username>/',UserPostListView.as_view(),name='user-posts'),
+    #path('user/<str:username>/',views.profilePost,name='user-posts'),
     path('post/delete/<int:pk>/',PostDeleteView.as_view(), name='post-delete'),
     path('',PostListView.as_view() ,name='blog-home'),
     path('post/<int:pk>/',PostDetailView.as_view() ,name='blog-home-post'),
