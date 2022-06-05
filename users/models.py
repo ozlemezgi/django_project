e7d8a43bf6c4dcd45122be16d9a31923557c18a4
ozlemezgi <ozlemezgisari@icloud.com>
2022-06-05 23:50:45 +0300

    fix toolbar

--- users/models.py
+++ users/models.py
@@ -4,12 +4,12 @@ from PIL import Image
 
 
 class Profile(models.Model):
-    GENDER_CHOICES = (('M', 'Male'),('F', 'Female'))
+    GENDER_CHOICES = (('Male', 'Male'),('Female', 'Female'))
     CITIES=(('nicosia','nicosia'),('famagusta','famagusta'),('kyrenia','kyrenia'),('omorfo','omorfo'),('lefka','lefka'))
-    PETS=(('petsareallowed','I have a pet or I am not against keeping pets at home.'),('petsarenotallowed',"I don't have a pet and I don't want any pets in my house."))
-    SMOKE=(("smokingisallowed","I smoke or I am not against smoking in the home."),("smokingisnotallowed" , "I don't smoke and I don't want smoking in my home."))
-    ALCHOOL=(("alchoolisallowed","I drink alcohol or I am not against alcohol consumption at home."),("alchoolisnotallowed","I do not drink alcohol and I do not want alcohol consumed in my home."))
-    HOME=(("hashome","I have a house and I am looking for a roommate for my house."),("nohome","I do not have a house, I am looking for a house and roommate."))
+    PETS=(('I have a pet or I am not against keeping pets at home.','I have a pet or I am not against keeping pets at home.'),("I don't have a pet and I don't want any pets in my house.","I don't have a pet and I don't want any pets in my house."))
+    SMOKE=(("I smoke or I am not against smoking in the home.","I smoke or I am not against smoking in the home."),("I don't smoke and I don't want smoking in my home.", "I don't smoke and I don't want smoking in my home."))
+    ALCHOOL=(("I drink alcohol or I am not against alcohol consumption at home.","I drink alcohol or I am not against alcohol consumption at home."),("I do not drink alcohol and I do not want alcohol consumed in my home.","I do not drink alcohol and I do not want alcohol consumed in my home."))
+    HOME=(("I have a house and I am looking for a roommate for my house.","I have a house and I am looking for a roommate for my house."),("I do not have a house, I am looking for a house and roommate.","I do not have a house, I am looking for a house and roommate."))
 
     user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
     image = models.ImageField(default='default.jpeg', upload_to='profile_pics')
